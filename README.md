d128db9bb257dc88ad10ea16e25c4833f9525629
smudiam <smudiam@gmail.com>
2023-06-27 15:11:41 -0700

    View slides with tiffslide package.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+### This program is to read the tiff file ausing tiffslide nd display region of interest using pillo packages.
+
+
+# Environment setup
+pip install -r requirements.txt
+
+# Run the Program
+python drawsvs.py --filename /Users/xxxxx/Folder1/Folder2/SVSFolder/testwsi.svs
---
+++ drawsvs.py
@@ -0,0 +1,38 @@
+import argparse
+import sys
+from tiffslide import TiffSlide
+from PIL import Image
+
+
+def display_slide(svsfile):
+    slide = TiffSlide(svsfile)
+
+    # check the # of levels/tiffs in the slide. Usually 4 with one for thumbnail.
+    dims = slide.level_dimensions
+    num_levels = len(dims)
+    print("Number of levels in this image are:", num_levels)
+    print("Dimensions of various levels in this image are:", dims)
+
+    # Selecting the region of the image to read and plot(pixels)
+    x1, x2 = 10000, 19000
+    y1, y2 = 10000, 19000
+
+    # Display the Region of interest
+    ROI_dim = dims[0]  # this images has only 1 dimension/level
+    ROI_img = slide.read_region((0, 0), 0, (x2, y2), as_array=True)
+    ROI_img = Image.fromarray(ROI_img[y1:y2, x1:x2, :])
+    ROI_img_RGB = ROI_img.convert('RGB')
+    ROI_img_RGB.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Specify the fully qualified Path of the SVS file: ')
+    parser.add_argument('--filename', type=str, help='filename')
+
+    args = parser.parse_args(sys.argv[1:])
+    svsfile = args.filename
+
+    if svsfile:
+        display_slide(svsfile)
+    else:
+        parser.print_help()
